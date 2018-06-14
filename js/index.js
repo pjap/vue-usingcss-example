87b568f2e93a2e55fe6685a543265587d3d7ab6e
pjap <pranajaya.pratama@gmail.com>
2018-06-14 14:56:13 +0700

    Create Example Style Dynamically Using CSS and Vue

--- js/index.js
+++ js/index.js
@@ -2,13 +2,21 @@ new Vue ({
   el : '#app',
   data : {
     title : 'Vue Example Using CSS',
-    attachRed : false
+    attachRed : false,
+    color : 'green',
+    width : 100
   },
   computed : {
     divClasses() {
       return {
         red: this.attachRed,
         blue: !this.attachRed,
+      }
+    },
+    myStyle() {
+      return {
+        backgroundColor: this.color,
+        width: this.width + 'px'
       }
     }
   }
